12a6991054a339c077a19c4eb8c73a59380f8516
Nusiba Ibrahim <ainusiba@gmail.com>
2020-07-19 07:01:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // Write your solution here!
 const append = ["Milo", "Otis", "Garfield"];
+append.puch("Odie") ;
+append;
 const prepend =  ["Milo", "Otis", "Garfield"];
 const removeLast = ["Milo", "Otis", "Garfield"];
 const removeFirst = ["Milo", "Otis", "Garfield"];

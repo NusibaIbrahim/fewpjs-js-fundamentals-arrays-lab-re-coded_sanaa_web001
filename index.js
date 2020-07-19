8a5f69508631597786ebdaaa47db9347f75935c2
Nusiba Ibrahim <ainusiba@gmail.com>
2020-07-19 06:52:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 // Write your solution here!
 const append = ["Milo", "Otis", "Garfield"];
+const append = ["Milo", "Otis", "Garfield"];
+
+const append = ["Milo", "Otis", "Garfield"];
+
+const append = ["Milo", "Otis", "Garfield"];
+

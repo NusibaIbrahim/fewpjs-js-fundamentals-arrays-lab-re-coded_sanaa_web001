5eccf8c79ac0c171d8c918b3ea03febe09017fda
Nusiba Ibrahim <ainusiba@gmail.com>
2020-07-19 07:01:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const prepend =  ["Milo", "Otis", "Garfield"];
 const removeLast = ["Milo", "Otis", "Garfield"];
 const removeFirst = ["Milo", "Otis", "Garfield"];
 
-append.puch("Odie") ;
 prepend.unshift("Odie") ;
 removeLast.pop() ;
 removeFirst.shift();
